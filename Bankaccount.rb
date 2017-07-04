9ee790627c857a1bd06928d3aad60a07637a71d1
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-04 16:57:39 -0400

    finished exercise 1

---
+++ Bankaccount.rb
@@ -0,0 +1,52 @@
+class BankAccount
+      @@interest_rate = 0.07
+      @@accounts = []
+
+      attr_reader :balance
+      attr_writer :balance
+
+  def initialize
+    @balance = 0
+  end
+
+
+  def deposit (deposit)
+    @balance += (deposit)
+  end
+
+  def withdraw (withdraw)
+    @balance -= (withdraw)
+  end
+
+
+
+  def self.create
+    @@accounts << BankAccount.new
+    return @@accounts.last
+  end
+
+  def self.total_funds
+    total = 0
+    @@accounts.each do |account|
+      total += account.balance
+    end
+    return total
+  end
+
+  def self.interest_times
+    @@accounts.each do |a|
+      a.balance = @@accounts *= (@@interest_rate + 1)
+    return a.balance
+  end
+  end
+
+
+  def print_balance
+    puts "Your balance is #{@balance}"
+  end
+end
+
+my_bankaccount = BankAccount.create
+savings_account = BankAccount.create
+puts my_bankaccount.balance
+puts savings_account.balance
